c7d4e5dfbec220a4db2d228e6bcc0f0db0df7603
Brian Tobal <brian.tobal@gmail.com>
2019-04-03 19:04:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array[-2].insert("and ")
+    array[-1].insert("and ")
     array.join(", ")
   else 
     array.join(" and ")
